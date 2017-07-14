a2c33dac25969f21eccc2174fa8deaa8b05535df
mathiasboeh <mathias.boehner@outlook.de>
2017-07-14 16:51:54 +0200

    Add files via upload

--- GameOfDice.js
+++ GameOfDice.js
@@ -9,56 +9,56 @@ document.getElementById("name2").innerHTML = "Player 2: " + Player2;
 
 
 //roll Player1
-function rollDice(){
-var dice1 = document.getElementById("dice1");
-var dice2 = document.getElementById("dice2");
-var dice3 = document.getElementById("dice3");
+function rollDice() {
+    var dice1 = document.getElementById("dice1");
+    var dice2 = document.getElementById("dice2");
+    var dice3 = document.getElementById("dice3");
     status1 = document.getElementById("status1"); //global
-var d1 = Math.floor(Math.random() * 6) + 1;
-var d2 = Math.floor(Math.random() * 6) + 1;
-var d3 = Math.floor(Math.random() * 6) + 1;
-    
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var d3 = Math.floor(Math.random() * 6) + 1;
+
     dice1.innerHTML = d1;
     dice2.innerHTML = d2;
     dice3.innerHTML = d3;
-    
-var diceTotal1 = d1 + d2 + d3;
 
-status1.innerHTML = "You rolled a " + diceTotal1 + "!";
+    var diceTotal1 = d1 + d2 + d3;
+
+    status1.innerHTML = "You rolled a " + diceTotal1 + "!";
 
 }
 
 //roll player2
-function rollDice2(){
-var dice4 = document.getElementById("dice4");
-var dice5 = document.getElementById("dice5");
-var dice6 = document.getElementById("dice6");
+function rollDice2() {
+    var dice4 = document.getElementById("dice4");
+    var dice5 = document.getElementById("dice5");
+    var dice6 = document.getElementById("dice6");
     status2 = document.getElementById("status2"); //global
-var d4 = Math.floor(Math.random() * 6) + 1;
-var d5 = Math.floor(Math.random() * 6) + 1;
-var d6 = Math.floor(Math.random() * 6) + 1;
-    
+    var d4 = Math.floor(Math.random() * 6) + 1;
+    var d5 = Math.floor(Math.random() * 6) + 1;
+    var d6 = Math.floor(Math.random() * 6) + 1;
+
     dice4.innerHTML = d4;
     dice5.innerHTML = d5;
     dice3.innerHTML = d6;
-    
+
     status2.innerHTML = "You rolled a " + diceTotal2 + "!";
 
 }
 
 //winner statement
-if (status1 > status2){
+if (status1 > status2) {
     document.getElementById('winner').innerHTML = "Player 1 is the winner!";
 }
-if (status1 < status2){
+if (status1 < status2) {
     document.getElementById('winner').innerHTML = "Player 2 is the winner!";
 }
-if (status1 == status2){
+if (status1 == status2) {
     document.getElementById('winner').innerHTML = "No winner!";
 }
 
 
-//randomColor
+//randomColor for the div inner. Copy/Paste from yesterday, not working anyways...
 function randomColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
